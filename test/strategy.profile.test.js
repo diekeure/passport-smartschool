05bc2c866a44be33e6b49a45187c8d83f6ac080f
Bert Casier <bert@ernie.be>
2016-03-23 16:54:28 +0100

    fix code coverage
    fix code coverage

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -76,5 +76,33 @@ describe('Strategy#userProfile', function() {
       expect(err.message).to.equal('Failed to parse user profile');
     });
   }); // error caused by malformed response
+
+  describe('error caused internally', function() {
+    var strategy =  new SmartschoolStrategy({
+        clientID: 'ABC123',
+        clientSecret: 'secret'
+      }, function() {});
+  
+    strategy._oauth2.get = function(url, accessToken, callback) {
+      var e = new Error("InternalServerError");
+      callback(e, undefined, undefined);
+    };
+    
+      
+    var err, profile;
+    
+    before(function(done) {
+      strategy.userProfile('token', function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('InternalServerError');
+    });
+  }); // error caused by malformed response
   
 });
